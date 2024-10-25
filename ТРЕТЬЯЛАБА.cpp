ed13982e2d85f1cac5e775cd50ed13bb298d9b11
BonTimofey <144820959+BonTimofey@users.noreply.github.com>
2024-10-25 12:07:46 +0500

    ТРЕТЬЯЛАБА.cpp

--- ТРЕТЬЯЛАБА.cpp
+++ ТРЕТЬЯЛАБА.cpp
@@ -13,20 +13,16 @@ private:
 
 public:
     Point(double x = 0, double y = 0) : x(x), y(y) {}
-
+// красивенький вывод
     string print() const {
         ostringstream oss;
         oss.precision(2);
         oss << fixed << "{" << x << ";" << y << "}";
         return oss.str();
     }
-    double getX() const { return x; }
-    double getY() const { return y; }
 
-    void setX(double x) { this->x = x; }
-    void setY(double y) { this->y = y; }
 };
-
+//создание точек и их вывод
 void task1() {
     Point p1(1, 2), p2(3.5, 4.6), p3(-1.2, 7.8);
     cout << "Точка 1: " << p1.print() << endl;
@@ -36,15 +32,16 @@ void task1() {
 
 // Задача 1.3: Имена
 class Name {
+// прописываем объекты, которые используем
 private:
     string surname;
     string firstName;
     string middleName;
-
+// прописываем переменные для имён
 public:
     Name(string surname = "", string firstName = "", string middleName = "")
         : surname(surname), firstName(firstName), middleName(middleName) {}
-
+// для красивого вывода
     string print() const {
         ostringstream oss;
         if (!surname.empty()) oss << surname;
@@ -69,22 +66,20 @@ class Line {
 private:
     Point start;
     Point end;
-
 public:
     Line(Point start, Point end) : start(start), end(end) {}
-
     string print() const {
         return "Линия от " + start.print() + " до " + end.print();
     }
-
     Point getStart() const { return start; }
     Point getEnd() const { return end; }
-
+    
     void setStart(Point p) { start = p; }
     void setEnd(Point p) { end = p; }
 };
 
 void task3() {
+    // создаём наши изначальные линии
     Point p1(1, 3), p2(23, 8), p3(5, 10), p4(25, 10);
 
     Line line1(p1, p2);
@@ -100,11 +95,23 @@ void task3() {
     line1.setEnd(Point(20, 10));
     line2.setStart(Point(10, 10));
     line2.setEnd(Point(30, 10));
+    line3.setStart(line1.getStart());
+    line3.setEnd(line2.getEnd());
 
     cout << "Измененные линии:" << endl;
     cout << line1.print() << endl;
     cout << line2.print() << endl;
+    cout << line3.print() << endl;
+    
+
+    line1.setStart(Point(50, 80));
+    line1.setEnd(Point(40, 50));
+    
+    cout << "ещё раз измененные линии:" << endl;
+    cout << line1.print() << endl;
+    cout << line2.print() << endl;
     cout << line3.print() << endl;  
+  
 }
 
 // Задача 3.3: Города
@@ -112,12 +119,9 @@ class City {
 private:
     string name;
     vector<pair<City*, int>> paths;
-
 public:
     City(const string& name) : name(name) {}
-
     City(const string& name, const vector<pair<City*, int>>& paths) : name(name), paths(paths) {}
-
     void addPath(City* Marshrut, int cost) {
         paths.push_back(make_pair(Marshrut, cost));
     }
